28dbfebe7ad36bc8ed0eabc4386dd68d6783b1c6
rayGreycloud <raygreycloud@gmail.com>
2022-01-30 09:32:20 -0800

    cleanup

--- server.js
+++ server.js
@@ -1,38 +1,15 @@
 const express = require('express');
-// const cron = require('node-cron');
 
 const { connectDB } = require('./services/mongodb');
-
-// const { checkUpdateStatusAndUpdateDb } = require('./controllers/update');
-// const updatingStatus = require('./config/keys').updatingStatus;
 const { updateDB } = require('./services/update-db');
 
 const app = express();
 
 connectDB();
 updateDB();
-// Cron - auto update stats/leaderboards
-// interval - every hour at *:59
-
-// Disable on scheduled update on test to prevent duplicating production update
-// cron.schedule('10 * * * * * *', async function () {
-//   console.log('updatingStatus: ', updatingStatus);
-//   if (updatingStatus == 'off') {
-//     console.log('Update disabled');
-//     return;
-//   } else {
-//     console.count('<==== Updating database ====>');
-//     console.log('timestamp: ', new Date());
-
-//     checkUpdateStatusAndUpdateDb();
-//   }
-// });
 
 // Initialize middleware
 app.use(express.json({ extended: false }));
-
-// Routes
-// app.use('/api/posts', require('./routes/api/posts'));
 
 // Set port for heroku or local
 const PORT = process.env.PORT || 5000;
