91347bb2e455bb6d3a55c3ca2d22ce173b146450
rayGreycloud <raygreycloud@gmail.com>
2022-01-30 10:05:18 -0800

    abstract cron schedule expression into var

--- services/update-db.js
+++ services/update-db.js
@@ -3,7 +3,13 @@ const cron = require('node-cron');
 const { checkUpdateStatusAndUpdateDb } = require('../controllers/update');
 const updatingDisabled = require('../config/keys').updatingDisabled;
 
-const task = cron.schedule('1 * * * *', async function () {
+// DEV ONLY: run update every minute
+const testingSchedule = '1 * * * * *';
+
+// NORMAL SCHEDULE: run update every hour at 1 minute past the hour
+const schedule = '1 * * * *';
+
+const task = cron.schedule(schedule, async function () {
   console.count('<==== Updating database ====>');
   console.log('timestamp: ', new Date());
 
